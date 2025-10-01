e7153417408e713f2d35ed104a042bca050a30e0
Student <bwilliams1@my.yearupunited.org>
2025-10-01 10:15:49 -0400

    Dialing Update

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -10,17 +10,43 @@ public class CellPhone {
     private String carrier;
     private String phoneNumber;
     private String owner;
-
-    public CellPhone (String serialNumber, String model, String carrier, String phoneNumber, String owner) {
+    private int selected;
+    private String receiverPhoneNumber;
+    public CellPhone () {
         this.serialNumber = serialNumber;
         this.model = model;
         this.carrier = carrier;
         this.phoneNumber = phoneNumber;
         this.owner = owner;
+        this.selected = selected;
+        this.receiverPhoneNumber = receiverPhoneNumber;
     }
 
-    public void display(String serialNumber, String model, String carrier, String phoneNumber, String owner) {
-        System.out.printf("Serial Number: %s\nModel: %s\nCarrier: %s\nPhone Number: %s\nOwner: %s", serialNumber, model, carrier, phoneNumber, owner);
+    public void display() {
+        System.out.println("CellPhoneApp [Open]");
+        System.out.print("Enter Serial Number: ");
+        serialNumber = keyboard.nextLine();
+
+        System.out.print("Enter the Model: ");
+        model = keyboard.nextLine();
+
+        System.out.print("Enter the Carrier: ");
+        carrier = keyboard.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        phoneNumber = keyboard.nextLine();
+
+        System.out.print("Enter Owners Name: ");
+        owner = keyboard.nextLine();
+
+        System.out.printf("Serial Number: %s\nModel: %s\nCarrier: %s\nPhone Number: %s\nOwner: %s\n\n", serialNumber, model, carrier, phoneNumber, owner);
+    }
+
+    public void dial() {
+        System.out.println("Enter Receiver's Phone Number: ");
+        receiverPhoneNumber = keyboard.nextLine();
+        System.out.printf("%s phone is calling %s\n", owner, receiverPhoneNumber);
+
     }
 
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -6,23 +6,8 @@ public class CellPhoneApplication {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Enter Serial Number: ");
-        String serialNumber = keyboard.nextLine();
-
-        System.out.println("Enter the Model: ");
-        String model = keyboard.nextLine();
-
-        System.out.println("Enter the Carrier: ");
-        String carrier = keyboard.nextLine();
-
-        System.out.println("Enter Phone Number");
-        String phoneNumber = keyboard.nextLine();
-
-        System.out.println("Enter Owners Name");
-        String owner = keyboard.nextLine();
-
-        CellPhone cellPhone = new CellPhone(serialNumber, model, carrier, phoneNumber, owner);
-
-        cellPhone.display(serialNumber, model, carrier, phoneNumber, owner);
+        CellPhone cellPhone = new CellPhone();
+        cellPhone.display();
+        cellPhone.dial();
     }
 }
